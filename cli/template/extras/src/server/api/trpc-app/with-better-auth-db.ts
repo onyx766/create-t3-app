5c99493b45e56352cd933255162e70939e4f5947
Charles Fries <charliefries@icloud.com>
2025-12-27 13:07:22 -0800

    Prettier CSS, @typescript-eslint/no-unnecessary-template-expression, @typescript-eslint/restrict-template-expressions

--- cli/template/extras/src/server/api/trpc-app/with-better-auth-db.ts
+++ cli/template/extras/src/server/api/trpc-app/with-better-auth-db.ts
@@ -97,7 +97,7 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
   const result = await next();
 
   const end = Date.now();
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+  console.log(`[TRPC] ${path} took ${String(end - start)}ms to execute`);
 
   return result;
 });
